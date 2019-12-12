26a32c045e7f88b0999c2745ff74b747d063070c
Nelly Barret <nelly.barret@etu.univ-lyon1.fr>
2019-12-12 18:04:18 +0100

    A: DQN

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # TP-IA5
+
+## Deep Q-network sur CartPole
+
+### Question 1
+
+Fichier correspondant : [cartPole-V1.py](cartPole-V1.py).
+
+### Question 2
+
+Pour cette question, nous avons utilisé `matplotlib`. Nous avons fait la somme des récompenses obtenues par l'agent pour chaque épisode. Nous avons calculé le nombre d'interactions de l'agent mais commme les récompenses valent 1, la moyenne de la somme des récompenses par la somme des interactions est de 1. 
+Comme l'agent ne meurt jamais, il obtient toujours des récompenses à 1.
+
+```shell script
+episode  927 , somme reward :  11.0  count_action: 11
+episode  942 , somme reward :  15.0  count_action: 15
+episode  983 , somme reward :  41.0  count_action: 41
+```
+
+
+![](qu2-1-2.png)
+
+
--- cartPole-V1.py
+++ cartPole-V1.py
@@ -1,56 +1,60 @@
-import argparse
-import sys
-
 import gym
-from gym import wrappers, logger
 import matplotlib.pyplot as plt
 
-class RandomAgent(object):
-    """The world's simplest agent!"""
+
+class ReplayAgent(object):
     def __init__(self, action_space):
         self.action_space = action_space
+        self.memory = []
 
     def act(self, observation, reward, done):
         return self.action_space.sample()
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=None)
-    parser.add_argument('env_id', nargs='?', default='CartPole-v0', help='Select the environment to run')
-    args = parser.parse_args()
 
-    # You can set the level to logger.DEBUG or logger.WARN if you
-    # want to change the amount of output.
-    logger.set_level(logger.INFO)
+env = gym.make("CartPole-v1")
+observation = env.reset()
+list_reward = []
+list_iteration = []
+somme_reward = 0
 
-    env = gym.make(args.env_id)
+agent = ReplayAgent(env.action_space)
+episode_count = 100
+reward = 0
+done = False
 
-    # You provide the directory to write to (can be an existing
-    # directory, including one with existing data -- all monitor files
-    # will be namespaced). You can also dump to a tempdir if you'd
-    # like: tempfile.mkdtemp().
-    outdir = '/tmp/random-agent-results'
-    env = wrappers.Monitor(env, directory=outdir, force=True)
-    env.seed(0)
-    agent = RandomAgent(env.action_space)
+for i in range(episode_count):
+    ob = env.reset()
+    while True:
+        action = agent.act(ob, reward, done)
+        ob, reward, done, _ = env.step(action)
+        if done:
+            print("episode ", i, ", somme reward : ", somme_reward)
+            list_reward.append(somme_reward)
+            list_iteration.append(i)
 
-    episode_count = 100
-    reward = 0
-    done = False
-    liste_rewards = []
+            break
 
-    for i in range(episode_count):
-        ob = env.reset()
-        while True:
-            action = agent.act(ob, reward, done)
-            ob, reward, done, _ = env.step(action)
-            liste_rewards.append(reward)
-            if done:
-                break
-            # Note there's no env.render() here. But the environment still can open window and
-            # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
-            # Video is not recorded every episode, see capped_cubic_video_schedule for details.
-    print(liste_rewards)
-    plt.plot(liste_rewards)
-    plt.show()
-    # Close the env and write monitor result info to disk
-    env.close()
+
+# for i in range(1000):
+#     env.render()
+#     action = agent.act(ob, reward, done) # your agent here (this takes random actions)
+#     observation, reward, done, info = env.step(action)
+#     somme_reward += reward
+#
+#     if done:
+#         print("episode ", i, ", somme reward : ", somme_reward)
+#         list_reward.append(somme_reward)
+#         list_iteration.append(i)
+#         somme_reward = 0
+#         observation = env.reset()
+
+fig = plt.figure()
+fig.subplots_adjust(top=0.8)
+ax1 = fig.add_subplot(111)
+ax1.set_ylabel('somme des récompenses')
+ax1.set_xlabel('épisode')
+ax1.set_title('cartPole - v1')
+ax1.plot(list_iteration, list_reward)
+
+plt.show()
+env.close()
